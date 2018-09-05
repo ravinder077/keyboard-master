8729e6c10c2120c8d73a7381b0c46d28b03d1327
ravinder077 <ravinder077@gmail.com>
2018-09-05 18:13:12 +0530

    last changes more

---
+++ app/src/main/java/fontconverter/com/cpkamboj/pupboard/FirstScreen.java
@@ -0,0 +1,54 @@
+package fontconverter.com.cpkamboj.pupboard;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class FirstScreen extends AppCompatActivity{
+    private Timer timer;
+    private ProgressBar progressBar;
+    private int i=0;
+    TextView textView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.first_screen);
+        progressBar=(ProgressBar)findViewById(R.id.progressBar);
+        progressBar.setProgress(0);
+        textView=(TextView)findViewById(R.id.textView);
+        textView.setText("");
+
+        final long period = 30;
+        timer=new Timer();
+        timer.schedule(new TimerTask() {
+            @Override
+            public void run() {
+                //this repeats every 100 ms
+                if (i<100){
+                    runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            //textView.setText(String.valueOf(i)+"%");
+                        }
+                    });
+                    progressBar.setProgress(i);
+                    i++;
+                }else{
+                    //closing the timer
+                    timer.cancel();
+                    Intent intent =new Intent(FirstScreen.this,MainActivity.class);
+                    startActivity(intent);
+                    // close this activity
+                    finish();
+                }
+            }
+        }, 0, period);
+    }
+
+}
